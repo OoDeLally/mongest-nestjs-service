2299e6945c7846058bdce7752ec68e21e89e55b5
Pascal Heitz <oodelally.npm@gmail.com>
2022-01-07 11:49:58 +0100

    npm build & publish settings

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { MongestService } from './MongestService';
+export { isEntityInstanceOf } from './polymorphism';
+export { registerEntityClassForSchema } from './registerEntityClassForSchema';
