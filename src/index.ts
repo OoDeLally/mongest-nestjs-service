59c7b13253bfee3cf188048466ba9986af189e55
Pascal Heitz <oodelally.npm@gmail.com>
2022-01-07 14:33:28 +0100

    export MongoProjection

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { BuildMongestService } from './BuildMongestService';
 export { MongestService } from './MongestService';
 export { isEntityInstanceOf } from './polymorphism';
+export { MongoProjection } from './projection';
 export { registerEntityClassForSchema } from './registerEntityClassForSchema';
